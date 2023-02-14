6aa0cd1a7b39c34c3d579f96e709bc1934a3d89e
kaylayshi <oparaiskelechi@gmail.com>
2023-02-14 23:51:56 +0100

    initial commit

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,13 @@
+package com.example
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.example.plugins.*
+
+fun main(args: Array<String>): Unit = EngineMain.main(args)
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
